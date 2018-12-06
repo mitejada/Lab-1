484986405323bc9c3d6fb3779a8b35c27c5b1828
Michell Tejada <michelltejada@pursuit.com>
2018-12-06 17:48:38 -0500

    Minor Updates

--- lab1.js
+++ lab1.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome'
+  })
+})
+
 app.get('/math/add/', (req, res) => {
   let reduced = Object.values(req.query).reduce(function(acc, element) {
     return acc = parseInt(acc) + parseInt(element)
@@ -21,7 +27,7 @@ app.get('/math/subtract', (req, res) => {
 
   let subtraction = {
     input: req.query,
-    diffString: Object.values(req.query).join(" - "),
+    differenceString: Object.values(req.query).join(" - "),
     difference: reduced
   }
 
@@ -33,13 +39,13 @@ app.get('/math/divide', (req, res) => {
     return acc = parseInt(acc) / parseInt(element)
   })
 
-  let quotient = {
+  let division = {
     input: req.query,
-    diffString: Object.values(req.query).join(" / "),
-    difference: reduced
+    quotientString: Object.values(req.query).join(" / "),
+    quotient: reduced
   }
 
-  res.json(quotient)
+  res.json(divison)
 })
 
 app.get('/math/multiply', (req, res) => {
@@ -47,17 +53,24 @@ app.get('/math/multiply', (req, res) => {
     return acc = parseInt(acc) *  parseInt(element)
   })
 
-  let product = {
+  let multiplication = {
     input: req.query,
-    diffString: Object.values(req.query).join(" * "),
-    difference: reduced
+    productString: Object.values(req.query).join(" * "),
+    product: reduced
   }
 
-  res.json(product)
+  res.json(multiplication)
 })
 
 app.get('/gif', (req, res) => {
+  let parsed = parseInt('http://api.giphy.com/v1/gifs/search?q=$%7BsearchTerm%7D&api_key=zjv0MwxwGDPD7yaSAQ3GQroMaDltB2mB')
 
+  console.log(parsed);
+
+  let display = [req.query.parsed]
+
+
+  res.json(display)
 })
 
 
